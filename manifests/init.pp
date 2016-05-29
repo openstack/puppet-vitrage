98cc8f89da71070ad90529225b7cc623466b7843
Eyal <eyal.bar-ilan@nokia.com>
2016-05-29 15:33:43 +0300

    Add vitrage graph class
    Change-Id: Ibc53be11ab7e0e0604e8b4cd3c140c17fb1d6ca7

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,19 @@
 #
 # === Parameters
 #
-class vitrage {
+# [*ensure_package*]
+#   (optional) The state of vitrage packages
+#   Defaults to 'present'
+#
+class vitrage (
+  $ensure_package  = 'present',
+) inherits vitrage::params {
 
-  include ::vitrage::params
+
+  package { 'vitrage':
+    ensure => $ensure_package,
+    name   => $::vitrage::params::common_package_name,
+    tag    => ['openstack', 'vitrage-package'],
+  }
 
 }
