6342209ac5306a6e98788d9614a5871ba9fccb0f
ZhongShengping <chdzsp@163.com>
2017-03-13 21:34:13 +0800

    Fix ci
    The review[0] is merged, we should update other releated
    puppet modules in order to fix ci.
    
    [0]https://review.openstack.org/#/c/444248/
    
    Change-Id: I3c9b3e042e7a11de8b3f044c5b6b91a76b49fe78
    Closes-Bug: #1672362

--- spec/classes/vitrage_init_spec.rb
+++ spec/classes/vitrage_init_spec.rb
@@ -126,8 +126,10 @@ describe 'vitrage' do
 
       it 'configures rabbit' do
         is_expected.to contain_vitrage_config('oslo_messaging_rabbit/rabbit_ha_queues').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_vitrage_config('oslo_messaging_rabbit/rabbit_use_ssl').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_vitrage_config('oslo_messaging_rabbit/amqp_durable_queues').with_value(true)
+        is_expected.to contain_oslo__messaging__rabbit('vitrage_config').with(
+          :rabbit_use_ssl     => '<SERVICE DEFAULT>',
+        )
       end
     end
 
@@ -141,11 +143,13 @@ describe 'vitrage' do
       end
 
       it 'configures rabbit' do
-        is_expected.to contain_vitrage_config('oslo_messaging_rabbit/rabbit_use_ssl').with_value(true)
-        is_expected.to contain_vitrage_config('oslo_messaging_rabbit/kombu_ssl_ca_certs').with_value('/etc/ca.cert')
-        is_expected.to contain_vitrage_config('oslo_messaging_rabbit/kombu_ssl_certfile').with_value('/etc/certfile')
-        is_expected.to contain_vitrage_config('oslo_messaging_rabbit/kombu_ssl_keyfile').with_value('/etc/key')
-        is_expected.to contain_vitrage_config('oslo_messaging_rabbit/kombu_ssl_version').with_value('TLSv1')
+        is_expected.to contain_oslo__messaging__rabbit('vitrage_config').with(
+          :rabbit_use_ssl     => true,
+          :kombu_ssl_ca_certs => '/etc/ca.cert',
+          :kombu_ssl_certfile => '/etc/certfile',
+          :kombu_ssl_keyfile  => '/etc/key',
+          :kombu_ssl_version  => 'TLSv1',
+        )
       end
     end
 
@@ -155,11 +159,13 @@ describe 'vitrage' do
       end
 
       it 'configures rabbit' do
-        is_expected.to contain_vitrage_config('oslo_messaging_rabbit/rabbit_use_ssl').with_value(true)
-        is_expected.to contain_vitrage_config('oslo_messaging_rabbit/kombu_ssl_ca_certs').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_vitrage_config('oslo_messaging_rabbit/kombu_ssl_certfile').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_vitrage_config('oslo_messaging_rabbit/kombu_ssl_keyfile').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_vitrage_config('oslo_messaging_rabbit/kombu_ssl_version').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_oslo__messaging__rabbit('vitrage_config').with(
+          :rabbit_use_ssl     => true,
+          :kombu_ssl_ca_certs => '<SERVICE DEFAULT>',
+          :kombu_ssl_certfile => '<SERVICE DEFAULT>',
+          :kombu_ssl_keyfile  => '<SERVICE DEFAULT>',
+          :kombu_ssl_version  => '<SERVICE DEFAULT>',
+        )
       end
     end
 
