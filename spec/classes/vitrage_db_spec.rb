6ffab7bb67ada59df5e38cb9e8061e2b7315c86a
ZhongShengping <chdzsp@163.com>
2017-04-11 14:34:49 +0800

    oslo db: check puppet resource instead of actual config in spec
    Change-Id: I1a87466f0706852865b62694cdcf5256a766d6b2

--- spec/classes/vitrage_db_spec.rb
+++ spec/classes/vitrage_db_spec.rb
@@ -4,14 +4,16 @@ describe 'vitrage::db' do
 
   shared_examples 'vitrage::db' do
     context 'with default parameters' do
-      it { is_expected.to contain_vitrage_config('database/connection').with_value('mysql://vitrage:secrete@localhost:3306/vitrage') }
-      it { is_expected.to contain_vitrage_config('database/idle_timeout').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_vitrage_config('database/min_pool_size').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_vitrage_config('database/db_max_retries').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_vitrage_config('database/max_retries').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_vitrage_config('database/retry_interval').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_vitrage_config('database/max_pool_size').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_vitrage_config('database/max_overflow').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_oslo__db('vitrage_config').with(
+        :db_max_retries => '<SERVICE DEFAULT>',
+        :connection     => 'mysql://vitrage:secrete@localhost:3306/vitrage',
+        :idle_timeout   => '<SERVICE DEFAULT>',
+        :min_pool_size  => '<SERVICE DEFAULT>',
+        :max_pool_size  => '<SERVICE DEFAULT>',
+        :max_retries    => '<SERVICE DEFAULT>',
+        :retry_interval => '<SERVICE DEFAULT>',
+        :max_overflow   => '<SERVICE DEFAULT>',
+      )}
     end
 
     context 'with specific parameters' do
@@ -27,19 +29,21 @@ describe 'vitrage::db' do
         }
       end
 
-      it { is_expected.to contain_vitrage_config('database/connection').with_value('mysql+pymysql://vitrage:vitrage@localhost/vitrage') }
-      it { is_expected.to contain_vitrage_config('database/idle_timeout').with_value('3601') }
-      it { is_expected.to contain_vitrage_config('database/min_pool_size').with_value('2') }
-      it { is_expected.to contain_vitrage_config('database/db_max_retries').with_value('-1') }
-      it { is_expected.to contain_vitrage_config('database/max_retries').with_value('11') }
-      it { is_expected.to contain_vitrage_config('database/retry_interval').with_value('11') }
-      it { is_expected.to contain_vitrage_config('database/max_pool_size').with_value('11') }
-      it { is_expected.to contain_vitrage_config('database/max_overflow').with_value('21') }
+      it { is_expected.to contain_oslo__db('vitrage_config').with(
+        :db_max_retries => '-1',
+        :connection     => 'mysql+pymysql://vitrage:vitrage@localhost/vitrage',
+        :idle_timeout   => '3601',
+        :min_pool_size  => '2',
+        :max_pool_size  => '11',
+        :max_retries    => '11',
+        :retry_interval => '11',
+        :max_overflow   => '21',
+      )}
     end
 
     context 'with postgresql backend' do
       let :params do
-        { :database_connection     => 'postgresql://vitrage:vitrage@localhost/vitrage', }
+        { :database_connection => 'postgresql://vitrage:vitrage@localhost/vitrage', }
       end
 
       it 'install the proper backend package' do
@@ -50,7 +54,7 @@ describe 'vitrage::db' do
 
     context 'with MySQL-python library as backend package' do
       let :params do
-        { :database_connection     => 'mysql://vitrage:vitrage@localhost/vitrage', }
+        { :database_connection => 'mysql://vitrage:vitrage@localhost/vitrage', }
       end
 
       it { is_expected.to contain_package('python-mysqldb').with(:ensure => 'present') }
@@ -58,7 +62,7 @@ describe 'vitrage::db' do
 
     context 'with incorrect database_connection string' do
       let :params do
-        { :database_connection     => 'redis://vitrage:vitrage@localhost/vitrage', }
+        { :database_connection => 'redis://vitrage:vitrage@localhost/vitrage', }
       end
 
       it_raises 'a Puppet::Error', /validate_re/
@@ -66,7 +70,7 @@ describe 'vitrage::db' do
 
     context 'with incorrect pymysql database_connection string' do
       let :params do
-        { :database_connection     => 'foo+pymysql://vitrage:vitrage@localhost/vitrage', }
+        { :database_connection => 'foo+pymysql://vitrage:vitrage@localhost/vitrage', }
       end
 
       it_raises 'a Puppet::Error', /validate_re/
@@ -76,7 +80,7 @@ describe 'vitrage::db' do
   shared_examples_for 'vitrage::db on Debian platforms' do
     context 'using pymysql driver' do
       let :params do
-        { :database_connection     => 'mysql+pymysql://vitrage:vitrage@localhost/vitrage', }
+        { :database_connection => 'mysql+pymysql://vitrage:vitrage@localhost/vitrage', }
       end
 
       it 'install the proper backend package' do
@@ -92,7 +96,7 @@ describe 'vitrage::db' do
   shared_examples_for 'vitrage::db on RedHat platforms' do
     context 'using pymysql driver' do
       let :params do
-        { :database_connection     => 'mysql+pymysql://vitrage:vitrage@localhost/vitrage', }
+        { :database_connection => 'mysql+pymysql://vitrage:vitrage@localhost/vitrage', }
       end
 
       it { is_expected.not_to contain_package('db_backend_package') }
@@ -100,7 +104,7 @@ describe 'vitrage::db' do
   end
 
   on_supported_os({
-    :supported_os   => OSDefaults.get_supported_os
+    :supported_os => OSDefaults.get_supported_os
   }).each do |os,facts|
     context "on #{os}" do
       let (:facts) do
