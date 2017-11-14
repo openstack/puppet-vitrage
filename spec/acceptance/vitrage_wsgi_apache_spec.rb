3b84dafbe9dca87c53f9e6a4ac42a43870e113f7
Mohammed Naser <mnaser@vexxhost.com>
2017-11-14 14:01:18 -0500

    Switch acceptance tests to use integration classes
    The integration classes contain much more up-to-date classes which
    means that it will prevent random failures.
    
    Depends-On: I2a5ae82bff051ac2a30a3d5447bb0b4b86dd1920
    Change-Id: Ib69111c6c5afa3d4c58abc7237237e1c8e90759d

--- spec/acceptance/vitrage_wsgi_apache_spec.rb
+++ spec/acceptance/vitrage_wsgi_apache_spec.rb
@@ -12,53 +12,12 @@ describe 'basic vitrage' do
       include ::openstack_integration::mysql
       include ::openstack_integration::keystone
 
-      rabbitmq_user { 'vitrage':
-        admin    => true,
-        password => 'an_even_bigger_secret',
-        provider => 'rabbitmqctl',
-        require  => Class['rabbitmq'],
-      }
-
-      rabbitmq_user_permissions { 'vitrage@/':
-        configure_permission => '.*',
-        write_permission     => '.*',
-        read_permission      => '.*',
-        provider             => 'rabbitmqctl',
-        require              => Class['rabbitmq'],
-      }
-
       case $::osfamily {
         'Debian': {
           warning('Vitrage is not yet packaged on Ubuntu systems.')
         }
         'RedHat': {
-
-          class { '::vitrage':
-            debug                 => true,
-            default_transport_url => 'rabbit://vitrage:an_even_bigger_secret@127.0.0.1:5672',
-          }
-          class { '::vitrage::keystone::auth':
-            password => 'a_big_secret',
-          }
-          class { '::vitrage::keystone::authtoken':
-            password => 'a_big_secret',
-          }
-          class { '::vitrage::api':
-            enabled      => true,
-            service_name => 'httpd',
-          }
-          include ::apache
-          class { '::vitrage::wsgi::apache':
-            ssl => false,
-          }
-          class { '::vitrage::auth':
-            auth_url      => 'http://127.0.0.1:5000/v3',
-            auth_password => 'a_big_secret',
-          }
-          class { '::vitrage::client': }
-          class { '::vitrage::notifier': }
-          class { '::vitrage::graph': }
-          class { '::vitrage::collector': }
+          include ::openstack_integration::vitrage
         }
         default: {
           fail("Unsupported osfamily (${::osfamily})")
