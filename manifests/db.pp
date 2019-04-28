a5010d2c4704a50c5c793a2d231c3f2fd400013d
ZhongShengping <chdzsp@163.com>
2019-04-28 15:00:14 +0800

    Deprecate idle_timeout option
    The idle_timeout parameter is deprecated, use connection_recycle_time
    instead[1].
    
    [1]https://review.opendev.org/#/c/334182/
    
    Change-Id: I3fc4376ba89ceb0ec26e67cde3746dae00c31b64
    Depends-On: https://review.opendev.org/656106/
    Closes-Bug: #1826692

--- manifests/db.pp
+++ manifests/db.pp
@@ -18,7 +18,7 @@
 #   before error is raised. Set to -1 to specify an infinite retry count.
 #   Defaults to $::os_service_default
 #
-# [*database_idle_timeout*]
+# [*database_connection_recycle_time*]
 #   (Optional) Timeout before idle SQL connections are reaped.
 #   Defaults to $::os_service_default.
 #
@@ -42,33 +42,47 @@
 #   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
 #   Defaults to $::os_service_default
 #
+# DEPRECATED PARAMETERS
+#
+# [*database_idle_timeout*]
+#   Timeout when db connections should be reaped.
+#   Defaults to undef.
+#
 class vitrage::db (
-  $database_connection     = 'mysql+pymysql://vitrage:secrete@localhost:3306/vitrage',
-  $database_idle_timeout   = $::os_service_default,
-  $database_min_pool_size  = $::os_service_default,
-  $database_max_pool_size  = $::os_service_default,
-  $database_db_max_retries = $::os_service_default,
-  $database_max_retries    = $::os_service_default,
-  $database_retry_interval = $::os_service_default,
-  $database_max_overflow   = $::os_service_default,
-  $database_pool_timeout   = $::os_service_default,
+  $database_connection              = 'mysql+pymysql://vitrage:secrete@localhost:3306/vitrage',
+  $database_connection_recycle_time = $::os_service_default,
+  $database_min_pool_size           = $::os_service_default,
+  $database_max_pool_size           = $::os_service_default,
+  $database_db_max_retries          = $::os_service_default,
+  $database_max_retries             = $::os_service_default,
+  $database_retry_interval          = $::os_service_default,
+  $database_max_overflow            = $::os_service_default,
+  $database_pool_timeout            = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $database_idle_timeout            = undef,
 ) {
 
   include ::vitrage::deps
+
+  if $database_idle_timeout {
+    warning('The database_idle_timeout parameter is deprecated. Please use \
+database_connection_recycle_time instead.')
+  }
+  $database_connection_recycle_time_real = pick($database_idle_timeout, $database_connection_recycle_time)
 
   validate_legacy(Oslo::Dbconn, 'validate_re', $database_connection,
     ['^(sqlite|mysql(\+pymysql)?|postgresql):\/\/(\S+:\S+@\S+\/\S+)?'])
 
   oslo::db { 'vitrage_config':
-    connection     => $database_connection,
-    idle_timeout   => $database_idle_timeout,
-    min_pool_size  => $database_min_pool_size,
-    max_pool_size  => $database_max_pool_size,
-    db_max_retries => $database_db_max_retries,
-    max_retries    => $database_max_retries,
-    retry_interval => $database_retry_interval,
-    max_overflow   => $database_max_overflow,
-    pool_timeout   => $database_pool_timeout,
+    connection              => $database_connection,
+    connection_recycle_time => $database_connection_recycle_time_real,
+    min_pool_size           => $database_min_pool_size,
+    max_pool_size           => $database_max_pool_size,
+    db_max_retries          => $database_db_max_retries,
+    max_retries             => $database_max_retries,
+    retry_interval          => $database_retry_interval,
+    max_overflow            => $database_max_overflow,
+    pool_timeout            => $database_pool_timeout,
   }
 
 }
