9796bfba7256a3071230e8c5ebfed0554ae9a2f9
ZhongShengping <chdzsp@163.com>
2020-03-25 14:53:41 +0800

    Deprecate min_pool_size option
    min_pool_size option is not used，see:
    
    https://review.opendev.org/#/c/565090/
    
    Change-Id: I046975163cd8f180760feb75dc8625a5fdcfafed
    Closes-Bug: #1868511

--- manifests/db.pp
+++ manifests/db.pp
@@ -26,10 +26,6 @@
 #   (optional) Interval between retries of opening a database connection.
 #   Defaults to $::os_service_default.
 #
-# [*database_min_pool_size*]
-#   (optional) Minimum number of SQL connections to keep open in a pool.
-#   Defaults to $::os_service_default.
-#
 # [*database_max_pool_size*]
 #   (optional) Maximum number of SQL connections to keep open in a pool.
 #   Defaults to $::os_service_default.
@@ -42,19 +38,30 @@
 #   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
 #   Defaults to $::os_service_default
 #
+# DEPRECATED PARAMETERS
+#
+# [*database_min_pool_size*]
+#   (optional) Minimum number of SQL connections to keep open in a pool.
+#   Defaults to undef.
+#
 class vitrage::db (
   $database_connection              = 'mysql+pymysql://vitrage:secrete@localhost:3306/vitrage',
   $database_connection_recycle_time = $::os_service_default,
-  $database_min_pool_size           = $::os_service_default,
   $database_max_pool_size           = $::os_service_default,
   $database_db_max_retries          = $::os_service_default,
   $database_max_retries             = $::os_service_default,
   $database_retry_interval          = $::os_service_default,
   $database_max_overflow            = $::os_service_default,
   $database_pool_timeout            = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $database_min_pool_size           = undef,
 ) {
 
   include vitrage::deps
+
+  if $database_min_pool_size {
+    warning('The database_min_pool_size parameter is deprecated, and will be removed in a future release.')
+  }
 
   validate_legacy(Oslo::Dbconn, 'validate_re', $database_connection,
     ['^(sqlite|mysql(\+pymysql)?|postgresql):\/\/(\S+:\S+@\S+\/\S+)?'])
@@ -62,7 +69,6 @@ class vitrage::db (
   oslo::db { 'vitrage_config':
     connection              => $database_connection,
     connection_recycle_time => $database_connection_recycle_time,
-    min_pool_size           => $database_min_pool_size,
     max_pool_size           => $database_max_pool_size,
     db_max_retries          => $database_db_max_retries,
     max_retries             => $database_max_retries,
