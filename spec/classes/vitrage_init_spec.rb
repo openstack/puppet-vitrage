108c225a340977db6b3f2917c40b8872a98812ad
ZhongShengping <chdzsp@163.com>
2020-11-02 14:42:04 +0800

    Deprecate allow_insecure_clients option
    The allow_insecure_clients has been deprecated[1].
    
    [1]https://review.opendev.org/#/c/417629/
    
    Change-Id: Icc4c4a30888b65c933b3de8d16bb01d957e05198
    Closes-Bug: #1902158

--- spec/classes/vitrage_init_spec.rb
+++ spec/classes/vitrage_init_spec.rb
@@ -186,7 +186,6 @@ describe 'vitrage' do
         is_expected.to contain_vitrage_config('oslo_messaging_amqp/ssl_cert_file').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_vitrage_config('oslo_messaging_amqp/ssl_key_file').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_vitrage_config('oslo_messaging_amqp/ssl_key_password').with_value('<SERVICE DEFAULT>')
-        is_expected.to contain_vitrage_config('oslo_messaging_amqp/allow_insecure_clients').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_vitrage_config('oslo_messaging_amqp/sasl_mechanisms').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_vitrage_config('oslo_messaging_amqp/sasl_config_dir').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_vitrage_config('oslo_messaging_amqp/sasl_config_name').with_value('<SERVICE DEFAULT>')
