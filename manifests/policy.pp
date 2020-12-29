892fdadf3f4abd127dd682bb874e605a7abe425c
Takashi Kajinami <tkajinam@redhat.com>
2020-12-29 17:02:28 +0900

    Use yaml instead of json for policy file
    Because usage of json for policy file will be deprecated and replaced
    by yaml[1].
    
    [1] https://governance.openstack.org/tc/goals/selected/wallaby/migrate-policy-format-from-json-to-yaml.html
    
    Depends-on: https://review.opendev.org/769647
    Change-Id: Iaf528055a56bd98b58815558bb1d8ae81f415217

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -20,12 +20,12 @@
 #   Defaults to empty hash.
 #
 # [*policy_path*]
-#   (Optional) Path to the nova policy.json file
-#   Defaults to /etc/vitrage/policy.json
+#   (Optional) Path to the nova policy.yaml file
+#   Defaults to /etc/vitrage/policy.yaml
 #
 class vitrage::policy (
   $policies    = {},
-  $policy_path = '/etc/vitrage/policy.json',
+  $policy_path = '/etc/vitrage/policy.yaml',
 ) {
 
   include vitrage::deps
@@ -34,9 +34,10 @@ class vitrage::policy (
   validate_legacy(Hash, 'validate_hash', $policies)
 
   Openstacklib::Policy::Base {
-    file_path  => $policy_path,
-    file_user  => 'root',
-    file_group => $::vitrage::params::group,
+    file_path   => $policy_path,
+    file_user   => 'root',
+    file_group  => $::vitrage::params::group,
+    file_format => 'yaml',
   }
 
   create_resources('openstacklib::policy::base', $policies)
