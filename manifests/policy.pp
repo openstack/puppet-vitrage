2a1b037cc792ad808c370bd83706e2a6c69374b3
Takashi Kajinami <tkajinam@redhat.com>
2021-03-21 11:05:03 +0900

    Add support for the oslo_policy/enforce_scope parameter
    Depends-on: https://review.opendev.org/#/c/759008/
    Change-Id: I8c823504fe0d42840bfa63311bcd0c03bac72b2a

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -4,6 +4,10 @@
 #
 # === Parameters
 #
+# [*enforce_scope*]
+#  (Optional) Whether or not to enforce scope when evaluating policies.
+#  Defaults to $::os_service_default.
+#
 # [*policies*]
 #   (Optional) Set of policies to configure for vitrage
 #   Example :
@@ -20,12 +24,13 @@
 #   Defaults to empty hash.
 #
 # [*policy_path*]
-#   (Optional) Path to the nova policy.yaml file
+#   (Optional) Path to the vitrage policy.yaml file
 #   Defaults to /etc/vitrage/policy.yaml
 #
 class vitrage::policy (
-  $policies    = {},
-  $policy_path = '/etc/vitrage/policy.yaml',
+  $enforce_scope = $::os_service_default,
+  $policies      = {},
+  $policy_path   = '/etc/vitrage/policy.yaml',
 ) {
 
   include vitrage::deps
@@ -42,5 +47,9 @@ class vitrage::policy (
 
   create_resources('openstacklib::policy::base', $policies)
 
-  oslo::policy { 'vitrage_config': policy_file => $policy_path }
+  oslo::policy { 'vitrage_config':
+    enforce_scope => $enforce_scope,
+    policy_file   => $policy_path
+  }
+
 }
--- spec/classes/vitrage_policy_spec.rb
+++ spec/classes/vitrage_policy_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe 'vitrage::policy' do
-
-  shared_examples_for 'vitrage policies' do
+  shared_examples 'vitrage::policy' do
     let :params do
       {
-        :policy_path => '/etc/vitrage/policy.yaml',
-        :policies    => {
+        :enforce_scope => false,
+        :policy_path   => '/etc/vitrage/policy.yaml',
+        :policies      => {
           'context_is_admin' => {
             'key'   => 'context_is_admin',
             'value' => 'foo:bar'
@@ -24,20 +24,21 @@ describe 'vitrage::policy' do
         :file_format => 'yaml',
       })
       is_expected.to contain_oslo__policy('vitrage_config').with(
-        :policy_file => '/etc/vitrage/policy.yaml',
+        :enforce_scope => false,
+        :policy_file   => '/etc/vitrage/policy.yaml',
       )
     end
   end
 
   on_supported_os({
-    :supported_os   => OSDefaults.get_supported_os
+    :supported_os => OSDefaults.get_supported_os
   }).each do |os,facts|
     context "on #{os}" do
       let (:facts) do
         facts.merge!(OSDefaults.get_facts())
       end
 
-      it_configures 'vitrage policies'
+      it_behaves_like 'vitrage::policy'
     end
   end
 end
