022fa51d91e6419749381a32855af21ee5142164
Takashi Kajinami <tkajinam@redhat.com>
2021-03-24 18:25:45 +0900

    Add support for oslo_policy/enforce_new_defaults
    Depends-on: https://review.opendev.org/781428
    Change-Id: If64c19c5dff5090a0804b23afe42e9821c9f622c

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -8,6 +8,11 @@
 #  (Optional) Whether or not to enforce scope when evaluating policies.
 #  Defaults to $::os_service_default.
 #
+# [*enforce_new_defaults*]
+#  (Optional) Whether or not to use old deprecated defaults when evaluating
+#  policies.
+#  Defaults to $::os_service_default.
+#
 # [*policies*]
 #   (Optional) Set of policies to configure for vitrage
 #   Example :
@@ -28,9 +33,10 @@
 #   Defaults to /etc/vitrage/policy.yaml
 #
 class vitrage::policy (
-  $enforce_scope = $::os_service_default,
-  $policies      = {},
-  $policy_path   = '/etc/vitrage/policy.yaml',
+  $enforce_scope        = $::os_service_default,
+  $enforce_new_defaults = $::os_service_default,
+  $policies             = {},
+  $policy_path          = '/etc/vitrage/policy.yaml',
 ) {
 
   include vitrage::deps
@@ -48,8 +54,9 @@ class vitrage::policy (
   create_resources('openstacklib::policy::base', $policies)
 
   oslo::policy { 'vitrage_config':
-    enforce_scope => $enforce_scope,
-    policy_file   => $policy_path
+    enforce_scope        => $enforce_scope,
+    enforce_new_defaults => $enforce_new_defaults,
+    policy_file          => $policy_path
   }
 
 }
--- spec/classes/vitrage_policy_spec.rb
+++ spec/classes/vitrage_policy_spec.rb
@@ -4,9 +4,10 @@ describe 'vitrage::policy' do
   shared_examples 'vitrage::policy' do
     let :params do
       {
-        :enforce_scope => false,
-        :policy_path   => '/etc/vitrage/policy.yaml',
-        :policies      => {
+        :enforce_scope        => false,
+        :enforce_new_defaults => false,
+        :policy_path          => '/etc/vitrage/policy.yaml',
+        :policies             => {
           'context_is_admin' => {
             'key'   => 'context_is_admin',
             'value' => 'foo:bar'
@@ -24,8 +25,9 @@ describe 'vitrage::policy' do
         :file_format => 'yaml',
       })
       is_expected.to contain_oslo__policy('vitrage_config').with(
-        :enforce_scope => false,
-        :policy_file   => '/etc/vitrage/policy.yaml',
+        :enforce_scope        => false,
+        :enforce_new_defaults => false,
+        :policy_file          => '/etc/vitrage/policy.yaml',
       )
     end
   end
