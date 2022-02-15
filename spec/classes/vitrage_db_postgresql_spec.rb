55d705c45d86fa78c5e43c3f2f843ee906b9c57c
Takashi Kajinami <tkajinam@redhat.com>
2022-02-16 00:30:27 +0900

    CentOS 9: Disable unit tests dependent on puppet-postgresql
    The puppt-postgresql module does not support CentOS 9 yet and requires
    some version parameters to be run on CentOS 9. This change disables
    unit tests requiring that module, until the module supports CentOS 9.
    
    Change-Id: Idcf8603a20feae912bf04e4332de85b04c4a5c21

--- spec/classes/vitrage_db_postgresql_spec.rb
+++ spec/classes/vitrage_db_postgresql_spec.rb
@@ -40,7 +40,10 @@ describe 'vitrage::db::postgresql' do
         }))
       end
 
-      it_configures 'vitrage::db::postgresql'
+      # TODO(tkajinam): Remove this once puppet-postgresql supports CentOS 9
+      unless facts[:osfamily] == 'RedHat' and facts[:operatingsystemmajrelease].to_i >= 9
+        it_configures 'vitrage::db::postgresql'
+      end
     end
   end
 
