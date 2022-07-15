f49cd06eeee3f1930ff2f8d7f1f27517689389f1
Takashi Kajinami <tkajinam@redhat.com>
2022-07-15 11:56:42 +0900

    Fix wrong test description
    The value with a white space should be accepted instead of being
    rejected. This change fixes the wrong test case description and makes
    sure the description explains what is actually tested.
    
    Change-Id: I2f840172b22928d136808568badd5135fc2b3fe3

--- spec/unit/type/vitrage_config_spec.rb
+++ spec/unit/type/vitrage_config_spec.rb
@@ -33,7 +33,7 @@ describe 'Puppet::Type.type(:vitrage_config)' do
     expect(@vitrage_config[:value]).to eq(['bar'])
   end
 
-  it 'should not accept a value with whitespace' do
+  it 'should accept a value with whitespace' do
     @vitrage_config[:value] = 'b ar'
     expect(@vitrage_config[:value]).to eq(['b ar'])
   end
